527dac00b2788d99af3bfc6638adae295b829b35
Steven <steven@binus.edu>
2025-05-22 14:36:25 +0700

    feat: add base path into /home

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import HomePage from './pages/HomePage'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/home">
       <Routes>
-        <Route path='/home' element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
