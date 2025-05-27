1b731a4631df55d340bc7b498682eda86fbc5cd8
Steven <steven@binus.edu>
2025-05-27 11:25:45 +0700

    revert: all back to default

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <BrowserRouter basename="/home">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
